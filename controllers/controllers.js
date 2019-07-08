ca33b7d23c8c0097e68c66fbb92722ef9c7bc50b
Luna Mancillas <lunamancillas@Lunas-MacBook-Pro.local>
2019-07-07 20:59:08 -0700

    this is very important routing to home and root

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -1,4 +1,5 @@
 var express = require("express");
+var path = require("path");
 
 var router = express.Router();
 
@@ -6,8 +7,12 @@ var router = express.Router();
 var cat = require("../models/index.js");
 
 // Create all our routes and set up logic within those routes where required.
-router.get("/", function(req, res) {
-  cat.all(function(data) {
+router.get("/home", function (req, res) {
+  res.sendFile(path.join(__dirname, "../public/blog.html"));
+});
+
+router.get("/", function (req, res) {
+  cat.all(function (data) {
     var hbsObject = {
       cats: data
     };
@@ -16,25 +21,25 @@ router.get("/", function(req, res) {
   });
 });
 
-router.post("/api/cats", function(req, res) {
+router.post("/api/cats", function (req, res) {
   cat.create([
     "name", "sleepy"
   ], [
-    req.body.name, req.body.sleepy
-  ], function(result) {
-    // Send back the ID of the new quote
-    res.json({ id: result.insertId });
-  });
+      req.body.name, req.body.sleepy
+    ], function (result) {
+      // Send back the ID of the new quote
+      res.json({ id: result.insertId });
+    });
 });
 
-router.put("/api/cats/:id", function(req, res) {
+router.put("/api/cats/:id", function (req, res) {
   var condition = "id = " + req.params.id;
 
   console.log("condition", condition);
 
   cat.update({
     sleepy: req.body.sleepy
-  }, condition, function(result) {
+  }, condition, function (result) {
     if (result.changedRows == 0) {
       // If no rows were changed, then the ID must not exist, so 404
       return res.status(404).end();
@@ -44,10 +49,10 @@ router.put("/api/cats/:id", function(req, res) {
   });
 });
 
-router.delete("/api/cats/:id", function(req, res) {
+router.delete("/api/cats/:id", function (req, res) {
   var condition = "id = " + req.params.id;
 
-  cat.delete(condition, function(result) {
+  cat.delete(condition, function (result) {
     if (result.affectedRows == 0) {
       // If no rows were changed, then the ID must not exist, so 404
       return res.status(404).end();
